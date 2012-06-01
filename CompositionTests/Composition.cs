0db8af8bc6fd14c26b8b0fa606fe859a7829cb39
Jim Counts <fritzjob18@nethere.com>
2012-06-01 07:00:40 -0700

    Added DiscoverParts and AssemblyCatalog Scrubbers
    DiscoverParts dumps and optionally scrubs CompositionInfo before
    Approving.
    ScrubPublicKeyToken removes the public key from the AssemblyCatalog
    signature, this helps make the output consistent between NCrunch and VS.
    ScrubVersionNumber removes the version number from the AssemblyCatalog
    signature, this helps make the output consistent when the version is
    automatically incremented.

---
+++ CompositionTests/Composition.cs
@@ -0,0 +1,45 @@
+﻿namespace CompositionTests
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+    using ApprovalTests;
+    using Microsoft.ComponentModel.Composition.Diagnostics;
+
+    public class Composition
+    {
+        public static void DiscoverParts(Func<CompositionInfo> getCompositionInfo, params Func<string, string>[] scrubbers)
+        {
+            try
+            {
+                using (var stringWriter = new StringWriter())
+                {
+                    CompositionInfoTextFormatter.Write(getCompositionInfo(), stringWriter);
+                    var result = stringWriter.ToString();
+                    foreach (var scrubber in scrubbers)
+                    {
+                        result = scrubber(result);
+                    }
+
+                    Approvals.Verify(result);
+                }
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                Array.ForEach(ex.LoaderExceptions, lex => Console.Error.WriteLine(lex.ToString()));
+                throw;
+            }
+        }
+
+        public static string ScrubPublicKeyToken(string value)
+        {
+            return new Regex(@",\sPublicKeyToken=[\dabcdefnul]+").Replace(value, string.Empty);
+        }
+
+        public static string ScrubVersionNumber(string value)
+        {
+            return new Regex(@",\sVersion=[\d.]+").Replace(value, string.Empty);
+        }
+    }
+}
