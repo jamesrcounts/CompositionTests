01a7deb7512904e33c665b740bde57c39c9ae4af
Jim Counts <fritzjob18@nethere.com>
2012-06-12 06:58:51 -0700

    Finished up Sample

--- Sample/CarDealership/CarDealership.Tests/IntegrationTest.cs
+++ Sample/CarDealership/CarDealership.Tests/IntegrationTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.ComponentModel.Composition.Hosting;
-using System.ComponentModel.Composition.Primitives;
-using System.IO;
-using System.Reflection;
-using ApprovalTests;
-using Microsoft.ComponentModel.Composition.Diagnostics;
+﻿using CompositionTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CarDealership.Tests
@@ -15,33 +9,7 @@ namespace CarDealership.Tests
     [TestMethod]
     public void DiscoverParts()
     {
-      DiscoverParts(Program.Catalog, Program.Host);
-    }
-
-    private static void DiscoverParts(ComposablePartCatalog catalog, ExportProvider host)
-    {
-      DiscoverParts(() => new CompositionInfo(catalog, host));
-    }
-
-    private static void DiscoverParts(Func<CompositionInfo> getCompositionInfo)
-    {
-      try
-      {
-        using (var stringWriter = new StringWriter())
-        {
-          CompositionInfoTextFormatter.Write(
-              getCompositionInfo(),
-              stringWriter);
-          Approvals.Verify(stringWriter.ToString());
-        }
-      }
-      catch (ReflectionTypeLoadException ex)
-      {
-        Array.ForEach(
-            ex.LoaderExceptions,
-            lex => Console.WriteLine(lex.ToString()));
-        throw;
-      }
+      Composition.DiscoverParts(Program.Catalog, StringExtensions.ScrubVersionNumber);
     }
   }
 }
